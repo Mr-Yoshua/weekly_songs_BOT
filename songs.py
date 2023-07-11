46e90c8f9be19c6c04edc1288e2908af8ca54e0a
Mr-Yoshua <j.lavadoortubia@gmail.com>
2023-07-11 16:52:52 -0400

    feat: se agrega loggin para mensajes en prod y mejora de funcion generate_list

--- songs.py
+++ songs.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import schedule
 import time
 from cron_job import change_priority_job
+import logging
 class Cancion:
     def __init__(self, nombre, clasificacion):
         self.nombre = nombre
@@ -20,10 +21,10 @@ def connect_bd():
     client = db_manager.client
     try:
         client.admin.command("ping")
-        print("Conexión exitosa a la base de datos")
+        logging.info("Conexión exitosa a la base de datos")
     except Exception as e:
-        print("Error al conectar a la base de datos")
-        print(e)
+        logging.error("Error al conectar a la base de datos")
+        logging.error(e)
     return db_manager
 
 def load_songs(list_name):
@@ -36,11 +37,12 @@ def load_songs(list_name):
     return songs
 
 def show_list(random_daily_songs):
-    print(f"--- Canciones para hoy elegidas al azar: ---")
+    logging.info(f"--- Canciones para hoy elegidas al azar: ---")
     for classification, songs in random_daily_songs.items():
         print(f"- {classification.upper()} -")
         for song in songs:
             print(f"Nombre: {song['name']} - Clasificación: {song['clasification']}")
+            logging.info(f"Nombre: {song['name']} - Clasificación: {song['clasification']}")
         print()
 
 
@@ -51,8 +53,7 @@ def generate_list(db_manager):
     for clasification in clasification_type:
         selected_songs = db_manager.get_song_by_priority("high")
         selected_songs = [song for song in selected_songs if song['clasification'] == clasification]
-        random.shuffle(selected_songs)  # Mezcla aleatoria de las canciones
-        random_songs = selected_songs[:2]  # Selecciona las primeras dos canciones de la lista mezclada
+        random_songs = random.sample(selected_songs, 2)  # Selecciona dos canciones aleatorias sin repetición
         random_daily_songs[clasification] = random_songs
 
     return random_daily_songs
@@ -62,7 +63,7 @@ def generate_list(db_manager):
 def populate_db(db_manager,songs):
     for song in songs:
         db_manager.insert_song({"name": song.nombre, "clasification": song.clasificacion, "priority": "high", "last_update": datetime.now()})
-    print("Se han insertado las canciones en la base de datos")
+    logging.info("Se han insertado las canciones en la base de datos")
 
 def run_cron_priority(db_manager):
     schedule.every().month.do(change_priority_job, db_manager)
@@ -79,15 +80,16 @@ async def send_by_telegram(random_daily_songs, bot_token, chat_id):
             message += f"Nombre: {song['name']}\n"
         message += "\n"
     await bot.send_message(chat_id=chat_id, text=message)
+    logging.info("Mensaje enviado por Telegram")
 
 async def main():
     load_dotenv()
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    CHAT_ID = os.getenv("CHAT_ID")
     db_instance = connect_bd()
     list_name = "song-list.json"
     songs = load_songs(list_name)
     populate_db(db_instance, songs) if not db_instance.collection_has_data() else None
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    CHAT_ID = os.getenv("CHAT_ID")
     random_daily_songs = generate_list(db_instance)
     show_list(random_daily_songs)
     await send_by_telegram(random_daily_songs, BOT_TOKEN, CHAT_ID)
